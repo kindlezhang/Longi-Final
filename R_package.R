2b88b15b8ca3ee254da20d1dcdffdc07224c3c79
Kindle zhang <qz2527@cumc.columbia.edu>
2024-12-02 00:08:20 -0500

    generate package

--- R_package.R
+++ R_package.R
@@ -7,3 +7,9 @@ install.packages("knitr")
 
 devtools::has_devel(debug = TRUE)
 pkgbuild::check_build_tools(debug = TRUE)
+
+# create package
+
+setwd(r"D:\Study\master lecture\longi data\final_project\Longi-Final")
+library(devtools)
+create_package("linear_model")
